87d35c5365f549dcdbc5fe5fe581703b1f6d89ca
PluseggWill <54594803+PluseggWill@users.noreply.github.com>
2020-04-21 02:41:37 -0400

    Update LevelOne

---
+++ Assets/Scripts/ReplacePrefab.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+
+public class ReplacePrefab : MonoBehaviour
+{
+    private string LoaclPath = "Assets/Prefabs/PlatformD";
+    public GameObject Object;
+    private void OnDrawGizmos() {
+        Object prefab = PrefabUtility.CreatePrefab(LoaclPath, )
+    }
+}
